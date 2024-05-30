4105e1ab84d1b719af8b7980ce618e339fd348c1
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-30 17:37:56 +0200

    to be fixed: transition to break length

--- pomodoro-timer/src/App.jsx
+++ pomodoro-timer/src/App.jsx
@@ -1,48 +1,112 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export default function App() {
 
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  const [timeLeft, setTimeLeft] = useState(`25:00`);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isTimeOver, setIsTimeOver] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(25);
+  const [timeLeft, setTimeLeft] = useState(`${minutes}:${seconds < 10? `0${seconds}`:`${seconds}`}`);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning && !isTimeOver) {
+      intervalId = setInterval(() => {
+
+        setSeconds(prevSeconds => {
+          if (prevSeconds === 0) return 59;
+          if (prevSeconds !== 0) return prevSeconds - 1;
+        });
+
+        setMinutes(prevMinutes => {
+          if (prevMinutes !== 0) return prevMinutes - 1;
+          if (prevMinutes === 0) {
+            setIsTimeOver(isTimeOver => !isTimeOver);
+            return;
+          }
+        });
+
+        setTimeLeft(`${minutes}:${seconds < 10? `0${seconds}`: `${seconds}`}`);
+
+      }, 1000);
+    } else if (isRunning && isTimeOver) {
+      console.log("time for a break");
+      // replace session time with break time
+      setMinutes(breakLength);
+      setTimeLeft(`${minutes}:${seconds < 10? `0${seconds}`:`${seconds}`}`);
+
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+
+  }, [isRunning, isTimeOver, minutes, seconds, timeLeft, breakLength]);
 
   const decrement = (e) => {
 
-    if (e.target.id === 'break-decrement') setBreakLength(prevBreakLength => {
-      return prevBreakLength === 1? prevBreakLength: prevBreakLength - 1;
-    });
-    if (e.target.id === 'session-decrement') setSessionLength(prevSessionLength => {
-      if (prevSessionLength === 1) return prevSessionLength;
-      const newSessionLength = prevSessionLength - 1;
-      setTimeLeft(`${newSessionLength}:00`);
-      return newSessionLength;
+    if (isRunning) return;
+
+    if (e.target.id === 'break-decrement') {
+      setBreakLength(prevBreakLength => {
+        return prevBreakLength === 1? prevBreakLength: prevBreakLength - 1;
       });
+    }
+    
+    if (e.target.id === 'session-decrement') {
+      setSessionLength(prevSessionLength => {
+        if (prevSessionLength === 1) return prevSessionLength;
+        if (prevSessionLength !== 1) return prevSessionLength - 1;
+      });
+      setMinutes(prevMinutes => {
+        if (prevMinutes === 1) return prevMinutes;
+        if (prevMinutes !== 1) {
+          const newMinutes = prevMinutes - 1;
+          setTimeLeft(`${newMinutes}:${seconds < 10? `0${seconds}`:`${seconds}`}`);
+          return newMinutes;
+        }
+      });
+    }
   }
 
   const increment = (e) => {
-    
+    if (isRunning) return;
     if (e.target.id === 'break-increment') setBreakLength(prevBreakLength => {
       return prevBreakLength === 60? prevBreakLength: prevBreakLength + 1;
     });
-    if (e.target.id === 'session-increment') setSessionLength(prevSessionLength => {
-      if (prevSessionLength === 60) return prevSessionLength;
-      const newSessionLength = prevSessionLength + 1;
-      setTimeLeft(`${newSessionLength}:00`);
-      return newSessionLength;
-    });
+    if (e.target.id === 'session-increment') {
+      setSessionLength(prevSessionLength => {
+        if (prevSessionLength === 60) return prevSessionLength;
+        if (prevSessionLength !== 60) return prevSessionLength + 1;
+      });
+      setMinutes(prevMinutes => {
+        if (prevMinutes === 60) return prevMinutes;
+        if (prevMinutes !== 60) {
+          const newMinutes = prevMinutes + 1;
+          setTimeLeft(`${newMinutes}:${seconds < 10? `0${seconds}`:`${seconds}`}`);
+          return newMinutes;
+        }
+      });
+    }
   }
 
   const reset = () => {
     setBreakLength(5);
     setSessionLength(25);
-    setTimeLeft(`25:00`);
+    setSeconds(0);
+    setMinutes(25);
+    setTimeLeft(`${25}:0${0}`);
+    if (isRunning) toggleTimer();
+    if (!isRunning) return;
   }
 
-  const handleTimer = () => {
-    console.log(timeLeft);
+  const toggleTimer = () => {
+    setIsRunning(prevState => !prevState);
   }
-
 
   return (
       <>
@@ -67,7 +131,7 @@ export default function App() {
         <div className='wrapper'>
             <label id='timer-label'>Session</label>
             <div id='time-left'>{timeLeft}</div>
-            <button id='start_stop' onClick={handleTimer}>Start/Stop</button>
+            <button id='start_stop' onClick={toggleTimer}>{isRunning? 'Pause': 'Start'}</button>
             <button id='reset' onClick={reset}>Reset</button>
             <audio id='beep'></audio>
         </div>
